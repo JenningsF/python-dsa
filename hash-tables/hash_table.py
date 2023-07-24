206e62c0ff65d398d5ff5810d6ac5678982f4695
JenningsF <6432715+JenningsF@users.noreply.github.com>
2023-07-24 13:39:53 -0500

    Added hash table constructor and print method

---
+++ hash-tables/hash_table.py
@@ -0,0 +1,20 @@
+class HashTable:
+    # constructor
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+    
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ": ", val)
+
+    def set_item(self, key, value):
+        pass
+
+    def get_item(self, key):
+        pass
---
+++ hash-tables/hash_table_practice.py
@@ -0,0 +1,5 @@
+from hash_table import HashTable
+
+my_ht = HashTable()
+
+my_ht.print_table()
