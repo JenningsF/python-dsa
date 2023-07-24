71709ea2498b63cae5f828648bce0fd0d24c4d96
JenningsF <6432715+JenningsF@users.noreply.github.com>
2023-07-24 12:56:41 -0500

    Added BST insert method

--- binary-search-tree/bst_practice.py
+++ binary-search-tree/bst_practice.py
@@ -1,5 +1,14 @@
 from binary_search_tree import BinarySearchTree
 
 # initial constructor of a Binary Search Tree (BST)
-my_bst = BinarySearchTree()
-print(my_bst.root)
+my_tree = BinarySearchTree()
+print(my_tree.root)
+
+# insert nodes into BST
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+
+print(my_tree.root.value)
+print(my_tree.root.left.value)
+print(my_tree.root.right.value)
