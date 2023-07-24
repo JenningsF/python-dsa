2f9a2ae08b5d151ad11324b0b3d37b496f8c1831
JenningsF <6432715+JenningsF@users.noreply.github.com>
2023-07-24 13:47:34 -0500

    Added set_item method

--- hash-tables/hash_table.py
+++ hash-tables/hash_table.py
@@ -3,18 +3,25 @@ class HashTable:
     def __init__(self, size = 7):
         self.data_map = [None] * size
 
+    # hash function
     def __hash(self, key):
         my_hash = 0
         for letter in key:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
     
+    # prints entire Hash Table
     def print_table(self):
         for i, val in enumerate(self.data_map):
             print(i, ": ", val)
 
+    # calculates and stores key-value pair at calculated address
     def set_item(self, key, value):
-        pass
+        index = self.__hash(key)
+        if self.data_map[index] is None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
 
+    # retrieves value based on specified key
     def get_item(self, key):
         pass
--- hash-tables/hash_table_practice.py
+++ hash-tables/hash_table_practice.py
@@ -1,5 +1,13 @@
 from hash_table import HashTable
 
+# initial HashTable constructor
 my_ht = HashTable()
+print("Initial Constructor")
+my_ht.print_table()
 
+# tests set_item method
+my_ht.set_item("bolts", 1400)
+my_ht.set_item("washers", 50)
+my_ht.set_item("lumber", 70)
+print("\nSet Item")
 my_ht.print_table()
