403abdeaf6ea2f7c974bc5645291c50de8977f98
JenningsF <6432715+JenningsF@users.noreply.github.com>
2023-07-25 09:39:12 -0500

    Added graph add_edge

--- graph/graph.py
+++ graph/graph.py
@@ -11,11 +11,19 @@ class Graph:
         if not self.adj_list:
             print(self.adj_list)
 
-
     # adds specified vertex to the adjacency list of Graph
     # if vertex already exists, then False is returned
     def add_vertex(self, vertex):
         if vertex not in self.adj_list.keys():
             self.adj_list[vertex] = []
+            return True
+        return False
+
+    # adds an edge between two specified vertices 
+    # if neither vertices exist, then False is returned
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
             return True
         return False
--- graph/graph_practice.py
+++ graph/graph_practice.py
@@ -5,7 +5,13 @@ my_graph = Graph()
 print("Initial constructor")
 my_graph.print_graph()
 
-# adds a vertex to graph
-my_graph.add_vertex('A')
-print("\nAdd vertex")
+# adds vertices to graph
+my_graph.add_vertex(1)
+my_graph.add_vertex(2)
+print("\nAdd vertices")
+my_graph.print_graph()
+
+# adds an edge to the graph
+my_graph.add_edge(1, 2)
+print("\nAdd edge")
 my_graph.print_graph()
