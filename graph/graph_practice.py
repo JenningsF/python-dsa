5cc1285d09714e1a22df1811c8113f1021cc83d2
JenningsF <6432715+JenningsF@users.noreply.github.com>
2023-07-25 09:43:58 -0500

    Added graph remove_edge

--- graph/graph_practice.py
+++ graph/graph_practice.py
@@ -6,12 +6,22 @@ print("Initial constructor")
 my_graph.print_graph()
 
 # adds vertices to graph
-my_graph.add_vertex(1)
-my_graph.add_vertex(2)
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_vertex('D')
 print("\nAdd vertices")
 my_graph.print_graph()
 
 # adds an edge to the graph
-my_graph.add_edge(1, 2)
-print("\nAdd edge")
+my_graph.add_edge('A', 'B')
+my_graph.add_edge('B', 'C')
+my_graph.add_edge('C', 'A')
+print("\nAdd edges")
+my_graph.print_graph()
+
+# removes an edge from the graph
+my_graph.remove_edge('A', 'B')
+my_graph.remove_edge('A', 'D')
+print("\nRemove edge")
 my_graph.print_graph()
